e914df089f683929c2ca1e309db7962da4c0e0a8
Dan802 <jfelipeg05@gmail.com>
2025-10-01 17:18:45 -0500

    Minor change: Información en la programación

--- src/components/FlechaAbajoSVG.tsx
+++ src/components/FlechaAbajoSVG.tsx
@@ -1,5 +1,5 @@
 export default function FlechaAbajoSVG() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M6 9l6 6 6-6"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M6 9l6 6 6-6"/></svg>
   )
 }
